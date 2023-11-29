8deca0c969243331466069cf252ef4ae61683c07
marceloabbadia <marcelo.abbadia@gmail.com>
2023-11-29 17:45:14 -0300

    Primeiro commit da aplicacao

---
+++ streams/fake-upload-to-http-stream.js
@@ -0,0 +1,27 @@
+import { Readable } from "node:stream";
+import fetch from "node-fetch";
+
+class OneToHundredStream extends Readable {
+  index = 1;
+
+  _read() {
+    const i = this.index++;
+    setTimeout(() => {
+      if (i > 3) {
+        this.push(null);
+      } else {
+        const buf = Buffer.from(String(i));
+        this.push(buf);
+      }
+    }, 1000);
+  }
+}
+
+fetch("http://localhost:3334", {
+  method: "POST",
+  body: new OneToHundredStream(),
+  duplex: true,
+}).then((response) => {
+  const data = response.text();
+  console.log(data);
+});
